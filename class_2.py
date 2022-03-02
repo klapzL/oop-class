b08cd2808202ef4bfe04d306f9135e9c163d8522
Emir <klerni89@gmail.com>
2022-03-02 19:57:18 +0600

    class inheritance

--- class_2.py
+++ class_2.py
@@ -1,34 +1,54 @@
 class Car:
     def __init__(self, max_speed, petrol_vol):
-        self.__max_speed = max_speed
+        self._max_speed = max_speed
         self.__petrol_vol = petrol_vol
-        self.__curr_speed = 0
+        self._current_speed = 0
 
-    def show_curr_speed(self):
-        return f'Текущая скорость равна: {self.__curr_speed}'
+    def _show_speed(self):
+        return f'Текущая скорость равна: {self._current_speed}'
 
     def increase_speed(self, val):
-        if self.__max_speed < int(val) + self.__curr_speed:
+        if self._max_speed < int(val) + self._current_speed:
             raise ValueError('Больше максимальной скорости')
         elif int(val) < 0:
             raise ValueError('Значение должно быть положительным')
         else: 
-            self.__curr_speed += int(val)
-            return 'Скорость повышена'
+            self._current_speed += int(val)
+            return f'Скорость повышена до {self._current_speed}'
 
     def decrease_speed(self, val):
-        if self.__curr_speed < int(val):
+        if self._current_speed < int(val):
             raise ValueError('Значение больше текущей скорости')
         elif int(val) < 0:
             raise ValueError('Значение должно быть положительным')
         else: 
-            self.__curr_speed -= int(val)
-            return 'Скорость понижена'
+            self._current_speed -= int(val)
+            return f'Скорость понижена до {self._current_speed}'
             
-car1 = Car(217, 90)
-val_increase = input()
-car1.increase_speed(val_increase)
-print(car1.show_curr_speed())
-val_decrease = input()
-car1.decrease_speed(val_decrease)
-print(car1.show_curr_speed())
+# car1 = Car(217, 90)
+# val_increase = input()
+# car1.increase_speed(val_increase)
+# print(car1.show_curr_speed())
+# val_decrease = input()
+# car1.decrease_speed(val_decrease)
+# print(car1.show_curr_speed())
+
+class Car_(Car):
+
+    def show_speed(self):
+        return self._current_speed
+
+    def change_speed(self, val):
+        if self._current_speed + val > self._max_speed:
+            raise ValueError('Скорость не может быть выше максимальной')
+        elif self._current_speed + val < 0:
+            self._current_speed = 0
+        else:
+            self._current_speed += val
+
+
+car2 = Car_(240,80)
+car2.change_speed(50)
+print(car2.show_speed())
+car2.change_speed(-750)
+print(car2.show_speed())
